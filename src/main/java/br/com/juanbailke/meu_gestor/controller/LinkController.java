ad6d7839dae5b6a29ce8ea50ec88783dbecfd40a
Juan <juan_bailke@hotmail.com>
2026-09-03 00:19:54 -0300

    Adicionados métodos de Put e Delete em LinkController.

--- src/main/java/br/com/juanbailke/meu_gestor/controller/LinkController.java
+++ src/main/java/br/com/juanbailke/meu_gestor/controller/LinkController.java
@@ -34,4 +34,33 @@ public class LinkController {
         Optional<Link> link = linkRepository.findById(id);
         return link.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Link> atualizarLink(@PathVariable Long id, @RequestBody Link linkAtualizado) {
+        return linkRepository.findById(id)
+                .map(linkExistente -> {
+                    linkExistente.setUrl(linkAtualizado.getUrl());
+                    linkExistente.setTitulo(linkAtualizado.getTitulo());
+                    linkExistente.setDescricao(linkAtualizado.getDescricao());
+                    linkExistente.setImagemCapa(linkAtualizado.getImagemCapa());
+                    linkExistente.setTempoEstimadoMinutos(linkAtualizado.getTempoEstimadoMinutos());
+                    linkExistente.setDataLimite(linkAtualizado.getDataLimite());
+                    linkExistente.setStatus(linkAtualizado.getStatus());
+                    linkExistente.setPrioridade(linkAtualizado.getPrioridade());
+                    linkExistente.setTags(linkAtualizado.getTags());
+
+                    linkRepository.save(linkExistente);
+                    return ResponseEntity.ok(linkExistente);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarLink(@PathVariable Long id) {
+        if (linkRepository.existsById(id)) {
+            linkRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
